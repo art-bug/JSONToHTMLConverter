8733ba4db8d2531aa23d07fd7916e1715837d856
art-bug <artbag94@gmail.com>
2021-08-20 05:20:49 +0300

    Task №3

--- json2html.py
+++ json2html.py
@@ -1,10 +1,13 @@
 '''
-     This module contains single json2html function
-     that converts JSON 'records'-like content to HTML string.
+     This module contains the json2html function and various auxiliary
+     functions and decorators for the task of
+     conversion JSON file 'records'-like content to a HTML list string.
 '''
 
 import argparse
 import os
+from collections.abc import Iterable
+import functools
 import json
 
 
@@ -16,13 +19,48 @@ def tag(html_tag: str, text: str):
     return f"<{html_tag}>{text}</{html_tag}>"
 
 
-def json2html(json_records: list):
+def record2html(json_record: dict):
     '''
-        Converts JSON file 'records'-like content to HTML string.
+        Converts JSON 'record' to a HTML string.
     '''
 
-    return ''.join(tag(html_tag, text) for record in json_records
-                   for html_tag, text in record.items())
+    return ''.join(tag(*item) for item in json_record.items())
+
+
+def html_list(html_elements: Iterable[str]):
+    '''
+        Represents the HTML elements as a HTML list.
+    '''
+
+    return tag('ul', ''.join(tag('li', el) for el in html_elements))
+
+
+def as_html_list(func):
+    '''
+        This decorator represents the HTML elements got from func
+        as a HTML list string.
+    '''
+
+    def wrapper(*args, **kwargs):
+        html_elements = func(*args, **kwargs)
+
+        return html_list(html_elements)
+
+    functools.update_wrapper(wrapper, func)
+
+    wrapper.__doc__ = "Converts JSON file 'records'-like content to "\
+                      "a HTML list string."
+
+    return wrapper
+
+
+@as_html_list
+def json2html(json_records: list):
+    '''
+        Converts JSON file 'records'-like content to list of HTML elements.
+    '''
+
+    return [record2html(record) for record in json_records]
 
 
 def __file_path(path: str):
@@ -35,7 +73,7 @@ def __file_path(path: str):
 
 
 def __parse_arguments():
-    description = "Convert JSON file content in 'records' format to HTML file."
+    description = "Convert JSON file in 'records' format to HTML file."
     argparser = argparse.ArgumentParser(description=description)
 
     input_help = "Input JSON file in 'records' format."
@@ -54,14 +92,14 @@ def __parse_arguments():
 
 
 if __name__ == "__main__":
-    args = __parse_arguments()
+    arguments = __parse_arguments()
 
     json_obj = {}
 
-    with open(args.input_json_file, 'r') as json_file:
+    with open(arguments.input_json_file, 'r') as json_file:
         json_obj = json.load(json_file)
 
     result_html = json2html(json_obj)
 
-    with open(args.output_html_file, 'w') as html_file:
+    with open(arguments.output_html_file, 'w') as html_file:
         html_file.write(result_html)
--- tests.py
+++ tests.py
@@ -1,15 +1,38 @@
-from json2html import json2html
+from json2html import record2html, json2html
 
 test_json_records = [
     {
         "h3": "Title #1",
         "div": "Hello, World 1!"
+    },
+    {
+        "h3": "Title #2",
+        "div": "Hello, World 2!"
     }
 ]
 
-required_html_string = "<h3>Title #1</h3><div>Hello, World 1!</div>"
+
+def test_record2html():
+    required_first_record_html = "<h3>Title #1</h3><div>Hello, World 1!</div>"
+
+    result_record_html = record2html(test_json_records[0])
+    assert result_record_html == required_first_record_html
+
+
+def test_wrapped_json2html():
+    required_list = ['<h3>Title #1</h3><div>Hello, World 1!</div>',
+                     '<h3>Title #2</h3><div>Hello, World 2!</div>']
+
+    orig_json2html = json2html.__wrapped__
+
+    result_list = orig_json2html(test_json_records)
+    assert result_list == required_list
 
 
 def test_json2html():
+    required_html = "<ul><li><h3>Title #1</h3><div>Hello, World 1!</div>"\
+                    "</li><li><h3>Title #2</h3><div>Hello, World 2!</div>"\
+                    "</li></ul>"
+
     result_html_string = json2html(test_json_records)
-    assert result_html_string == required_html_string
+    assert result_html_string == required_html
